9ee54fa1f17c793b7204e7d3f93daea082a9c1c6
Ekaterina Khmaruk <ekaterinabond43@gmail.com>
2022-11-15 09:52:10 +0500

    Created repos

---
+++ 1.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+
+Console.Write("Введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+Console.WriteLine("Максимальное число " + a );
+Console.WriteLine("Минимальное число " + b );
+
+}
+else {
+    Console.WriteLine("Максимальное число " + b );
+    Console.WriteLine("Минимальное число " + a );
+}
